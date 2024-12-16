364e232046467186feb7c12ea1a8b5661bc70da1
Ahmed Abdelsalam <hamoody347@gmail.com>
2024-12-16 11:01:22 +0400

    creds

--- src/app/[locale]/auth/login/page.jsx
+++ src/app/[locale]/auth/login/page.jsx
@@ -129,7 +129,7 @@ export default function Login() {
                                         required
                                     />
                                     <Link href="/auth/forgotpassword">
-                                        {t('Forgot Password')} {' ? '} 
+                                        {t('Forgot Password')} {' ? '}
                                     </Link>
                                 </div>
                                 <div className="buttons mt-3 mb-1">
