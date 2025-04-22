e778b73996c4d8edd7c2b9a76164a4ad457ff7cd
Ahmed Abdelsalam <hamoody347@gmail.com>
2025-04-22 11:58:45 +0400

    Disabled double hits for use effect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, // Disable React Strict Mode in development
+    reactStrictMode: true, // Disable React Strict Mode in development
 };
 
 export default nextConfig;
