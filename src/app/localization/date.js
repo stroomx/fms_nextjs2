80353a34b90b1e51fb82a9a5327a25fa60f6d62e
Ahmed Abdelsalam <hamoody347@gmail.com>
2025-06-03 15:25:34 +0400

    Added exception handling for the Date and Time functions, this needs to be revisited again

--- src/app/localization/date.js
+++ src/app/localization/date.js
@@ -1,29 +1,33 @@
 export default function formatDate(dateStr, showTime = true, locale = navigator.language) {
     if (!dateStr)
-        return;
-    
-    // Parse the ISO formatted date string into a Date object
-    const date = new Date(dateStr + "Z");
+        return '';
 
-    // Check if the date is valid
-    if (isNaN(date)) {
-        throw new Error("Invalid date format");
+    try {
+        // Parse the ISO formatted date string into a Date object
+        const date = new Date(dateStr + "Z");
+
+        // Check if the date is valid
+        if (isNaN(date)) {
+            throw new Error("Invalid date format");
+        }
+
+        // Create date formatting options based on whether time should be included
+        const options = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: showTime ? 'numeric' : undefined,
+            minute: showTime ? 'numeric' : undefined,
+            second: showTime ? 'numeric' : undefined,
+            hour12: false, // Use 24-hour clock by default
+            timeZone: undefined // (User settgins first) if not set ( Franchise for the Schedule Timezone Second ) // Keep as franchise
+        };
+
+        // Format the date according to the specified locale
+        const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
+
+        return formattedDate;
+    } catch (error) {
+        return '';
     }
-
-    // Create date formatting options based on whether time should be included
-    const options = {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: showTime ? 'numeric' : undefined,
-        minute: showTime ? 'numeric' : undefined,
-        second: showTime ? 'numeric' : undefined,
-        hour12: false, // Use 24-hour clock by default
-        timeZone: undefined // (User settgins first) if not set ( Franchise for the Schedule Timezone Second ) // Keep as franchise
-    };
-
-    // Format the date according to the specified locale
-    const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
-
-    return formattedDate;
 }
--- src/app/localization/time.js
+++ src/app/localization/time.js
@@ -1,29 +1,33 @@
 export default function formatTime(time, locale = navigator.language) {
     if (!time)
-        return;
+        return '';
 
-    // Parse the ISO formatted date string into a Date object
-    const date = new Date('2000-01-01 ' + time + "Z");
+    try {
+        // Parse the ISO formatted date string into a Date object
+        const date = new Date('2000-01-01 ' + time + "Z");
 
-    // Check if the date is valid
-    if (isNaN(date)) {
-        throw new Error("Invalid date format");
+        // Check if the date is valid
+        if (isNaN(date)) {
+            throw new Error("Invalid date format");
+        }
+
+        // Create date formatting options based on whether time should be included
+        const options = {
+            year: undefined,
+            month: undefined,
+            day: undefined,
+            hour: 'numeric',
+            minute: 'numeric',
+            second: undefined,
+            hour12: false, // Use 24-hour clock by default
+            timeZone: undefined // (User settgins first) if not set ( Franchise for the Schedule Timezone Second ) // Keep as franchise
+        };
+
+        // Format the date according to the specified locale
+        const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
+
+        return formattedDate;
+    } catch (error) {
+        return '';
     }
-
-    // Create date formatting options based on whether time should be included
-    const options = {
-        year: undefined,
-        month: undefined,
-        day: undefined,
-        hour: 'numeric',
-        minute: 'numeric',
-        second: undefined,
-        hour12: false, // Use 24-hour clock by default
-        timeZone: undefined // (User settgins first) if not set ( Franchise for the Schedule Timezone Second ) // Keep as franchise
-    };
-
-    // Format the date according to the specified locale
-    const formattedDate = new Intl.DateTimeFormat(locale, options).format(date);
-
-    return formattedDate;
 }
