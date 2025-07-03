eb98870e66813aca7af346359c4a6ba9a2e85aad
Ajmal Alavi <100898675+ajmalalavi@users.noreply.github.com>
2025-07-03 23:02:51 +0400

    Update modal.jsx

--- src/app/[locale]/auth/login/modal.jsx
+++ src/app/[locale]/auth/login/modal.jsx
@@ -27,7 +27,6 @@ export default function LoginModal({ franchise_id, schedule_id, checkout = true 
             try {
                 const { data } = await axiosInstance.get('/api/parentregistration.php');
                 setPolicies(data);
-                console.log(data);
             } catch (err) {
                 console.log(err);
             }
